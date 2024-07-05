3c4340f08629956a70f4965ac8b0fd1aaec15090
Damien <dwrig28@gmail.com>
2024-07-05 18:57:09 -0400

    Editing Players df to be from nfl_data_py

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ from io import BytesIO
 from PIL import Image
 import pandas as pd
 import os
+import nfl_data_py as nfl
+
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Load data
-players = pd.read_csv('roster_2024.csv')
-players['name/team/pos'] = players['full_name'] + ' ' + players['team'] + ' ' + players['position']
+players = nfl.import_players()
+players = players[(players['status']=='ACT') & players['position'].isin(['QB','RB','WR','TE'])]
+players['name/team/pos'] = players['display_name'] + ' ' + players['team_abbr'] + ' ' + players['position']
 
 df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
@@ -18,117 +21,85 @@ df = pd.DataFrame({
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
 
-# Function to retrieve image URL from website based on player name
-def get_image_url(player_name):
-    image_url = players[players['name/team/pos'] == player_name]['headshot_url'].unique()[0]
-
-    # Send a GET request to fetch the raw image data
-    response = requests.get(image_url)
-            
-    # Check if the request was successful (status code 200)
-    
-    if response.status_code == 200:
-        # Return player name and image as HTML elements
-        return [None, html.Img(src=image_url, style={'width': '100%', 'height': 'auto', 'maxWidth': '325px', 'maxHeight': '232px'})]
-    else:
-        return f"Failed to retrieve the image for {player_name}. Status code: {response.status_code}", html.Div()
-
-
 # App layout
 app.layout = html.Div([
     html.H1(children='Fantasy Football Scoring App', style={'textAlign': 'center'}),
     html.H2(children='Enter your custom scoring settings below.', style={'textAlign': 'left'}),
     dbc.Row(
         [
-        dbc.Col(
-            [   
-                dbc.Row(
-                    html.Div('Passing',style={'textAlign': 'left',"text-decoration": "underline"})
-                        ),
-                dbc.Row(
-                    [
-                        html.Div("Passing TD pts", style={'marginRight': '10px', 'display': 'inline-block', 'width': '120px'}),
-                        dcc.Input(id="pass_tds", type="number", placeholder="", style={'width': '100px', 'display': 'inline-block'})
-                    ],
-                    className="p-0"  # Adjust horizontal padding to reduce space between columns
-                ),
-                dbc.Row(
-                    [
-                        html.Div("Passing YDs (per 1 yard)", style={'marginRight': '10px', 'display': 'inline-block', 'width': '120px'}),
-                        dcc.Input(id="pass_yds", type="number", placeholder="", style={'width': '100px', 'display': 'inline-block'})
-                    ],
-                    className="p-0"  # Adjust horizontal padding to reduce space between columns
-                ),
-                dbc.Row(
-                    [
-                        html.Div("Interceptions", style={'marginRight': '10px', 'display': 'inline-block', 'width': '120px'}),
-                        dcc.Input(id="ints", type="number", placeholder="", style={'width': '100px', 'display': 'inline-block'})
-                    ],
-                    className="p-0"  # Adjust horizontal padding to reduce space between columns
-                ),
-            ],
-        ),
-        dbc.Col(
-            [
-                dbc.Row(
-                    html.Div('Rushing',style={'textAlign': 'left',"text-decoration": "underline"})
-                        ),
-                dbc.Row(
-                    [
-                        html.Div("Rushing TD pts", style={'marginRight': '10px', 'display': 'inline-block', 'width': '120px'}),
-                        dcc.Input(id="pass_tds", type="number", placeholder="", style={'width': '100px', 'display': 'inline-block'})
-                    ],
-                    className="p-0"  # Adjust horizontal padding to reduce space between columns
-                ),
-                dbc.Row(
-                    [
-                        html.Div("Rushing YDs (per 1 yard)", style={'marginRight': '10px', 'display': 'inline-block', 'width': '120px'}),
-                        dcc.Input(id="pass_yds", type="number", placeholder="", style={'width': '100px', 'display': 'inline-block'})
-                    ],
-                    className="p-0"  # Adjust horizontal padding to reduce space between columns
-                ),
-                dbc.Row(
-                    [
-                        html.Div("Interceptions", style={'marginRight': '10px', 'display': 'inline-block', 'width': '120px'}),
-                        dcc.Input(id="ints", type="number", placeholder="", style={'width': '100px', 'display': 'inline-block'})
-                    ],
-                    className="p-0"  # Adjust horizontal padding to reduce space between columns
-                ),
-            ],
-        ),
-    ]),
-
-
-
-
-    # dbc.Container([
-    # html.H3(children='Passing', style={'textAlign': 'left',"text-decoration": "underline"}),
-    # dbc.Stack(
-    #     [
-    #         html.Div("Pass TDs"),
-    #         dcc.Input(id="Pass Tds", type="text", placeholder="",style={'width':20}),
-    #         html.Div("Pass YDs"),
-    #         dcc.Input(id="Pass YDs", type="text", placeholder="",style={'width':20})
-
-
-    #     ],
-    #     direction="horizontal", gap=1),
-    # ], style={'textAlign': 'left'}),
-
-    # dbc.Container([
-    # html.H3(children='Rushing', style={'textAlign': 'left',"text-decoration": "underline"}),
-    # dbc.Stack(
-    #     [
-    #         html.Div("Rushing TDs"),
-    #         dcc.Input(id="Rushing Tds", type="text", placeholder="",style={'width':20}),
-    #         html.Div("Rushing YDs"),
-    #         dcc.Input(id="Rushing YDs", type="text", placeholder="",style={'width':20})
-
-
-    #     ],
-    #     direction="horizontal", gap=1),
-    # ], style={'textAlign': 'left'}),
-            
+            dbc.Col(
+                [
+                    dbc.Row(html.Div('Passing', style={'textAlign': 'left', "text-decoration": "underline"})),
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Div("Passing TD pts", style={'white-space': 'nowrap'}), width=6),
+                            dbc.Col(dcc.Input(id="pass_tds", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                        ],
+                        className="mb-3"
+                    ),
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Div("Passing YDs (per 1 yard)", style={'white-space': 'nowrap'}), width=6),
+                            dbc.Col(dcc.Input(id="pass_yds", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                        ],
+                        className="mb-3"
+                    ),
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Div("Interceptions", style={'white-space': 'nowrap'}), width=6),
+                            dbc.Col(dcc.Input(id="ints", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                        ],
+                        className="mb-3"
+                    ),
+                ],
+            ),
+            dbc.Col(
+                [
+                    dbc.Row(html.Div('Rushing', style={'textAlign': 'left', "text-decoration": "underline"})),
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Div("Rushing TD pts", style={'white-space': 'nowrap'}), width=6),
+                            dbc.Col(dcc.Input(id="rush_tds", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                        ],
+                        className="mb-3"
+                    ),
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Div("Rushing YDs (per 1 yard)", style={'white-space': 'nowrap'}), width=6),
+                            dbc.Col(dcc.Input(id="rush_yds", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                        ],
+                        className="mb-3"
+                    ),
+                ]
+            ),
+            dbc.Col(
+                [
+                    dbc.Row(html.Div('Receiving', style={'textAlign': 'left', "text-decoration": "underline"})),
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Div("Receiving TD pts", style={'white-space': 'nowrap'}), width=6),
+                            dbc.Col(dcc.Input(id="rec_tds", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                        ],
+                        className="mb-3"
+                    ),
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Div("Receiving YDs (per 1 yard)", style={'white-space': 'nowrap'}), width=6),
+                            dbc.Col(dcc.Input(id="rec_yds", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                        ],
+                        className="mb-3"
+                    ),
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Div("Receptions (ppr)", style={'white-space': 'nowrap'}), width=6),
+                            dbc.Col(dcc.Input(id="recs", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                        ],
+                        className="mb-3"
+                    ),
+                ]
+            ),
+        ]
+    ),
     html.H2('Enter a player\'s name below'),
     dcc.Dropdown(
         id='player-dropdown', 
@@ -168,24 +139,19 @@ def update_output(n_clicks, player_name):
     if not player_name:
         return "No Player Selected", html.Div()  # Return empty div for image container if no player name
     else:
-        image_url=players[players['name/team/pos']==player_name]['headshot_url'].unique()[0]
-        if image_url is not None:
-             # Send a GET request to fetch the raw image data
+        image_url=players[players['name/team/pos']==player_name]['headshot'].unique()[0]
+        if image_url:
             response = requests.get(image_url)
-            
-            # Check if the request was successful (status code 200)
             if response.status_code == 200:
-                # Extract the raw image content
                 image_content = response.content
-                
-                # Use PIL to open the image from the bytes
                 image = Image.open(BytesIO(image_content))
-                
-                # Return player name and image as HTML elements
                 return f"You entered: {player_name}", html.Img(src=image_url, style={'width': '325px', 'height': '232px'})
-            
             else:
                 return f"Failed to retrieve the image for {player_name}. Status code: {response.status_code}", html.Div()
+
+        else:
+            return f"{player_name} does not have a headshot.", html.Div()
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8050))
     app.run_server(host='0.0.0.0', port=port)
