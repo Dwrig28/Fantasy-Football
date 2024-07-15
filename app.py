ae346887e2e1302a1385132cdfb947bbf1938a76
Damien <dwrig28@gmail.com>
2024-07-15 16:47:11 -0400

    updating visual elements adding fantasypro projections

--- app.py
+++ app.py
@@ -4,19 +4,40 @@ import requests
 import pandas as pd
 import os
 import nfl_data_py as nfl
-
+import warnings
+warnings.filterwarnings("ignore")
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Load data
 players = nfl.import_players()
-players = players[(players['status']=='ACT') & players['position'].isin(['QB','RB','WR','TE'])]
+players = players[(players['status'] == 'ACT') & players['position'].isin(['QB', 'RB', 'WR', 'TE'])]
 players['name/team/pos'] = players['display_name'] + ' ' + players['team_abbr'] + ' ' + players['position']
 
-df=pd.DataFrame(columns=['season','passing_yards', 'rushing_yards', 'receiving_yards', 'passing_tds', 'rushing_tds', 'receiving_tds','interceptions','fumbles_lost'])
+seasonal_data = nfl.import_seasonal_data([2018, 2019, 2020, 2021, 2022, 2023], 'REG')
+seasonal_data['fumbles_lost'] = seasonal_data['sack_fumbles_lost'] + seasonal_data['rushing_fumbles_lost'] + seasonal_data['receiving_fumbles_lost']
+seasonal_data.rename(columns={'season':'Season','passing_yards':'Passing Yds','passing_tds':'Passing TDs','interceptions':'INTs','rushing_yards':'Rushing Yds','rushing_tds':'Rushing TDs','fumbles_lost':'Fumbles','receiving_yards':'Receiving Yds','receiving_tds':'Receiving TDs','receptions':'Receptions'},inplace=True)
 
-seasonal_data=nfl.import_seasonal_data([2018,2019,2020,2021,2022,2023],'REG')
-seasonal_data['fumbles_lost']=seasonal_data['sack_fumbles_lost'] + seasonal_data['rushing_fumbles_lost'] +seasonal_data['receiving_fumbles_lost']
+# Projections data from FantasyPros
+url_fp_qb = "https://www.fantasypros.com/nfl/projections/qb.php?week=draft"
+fp_qb = pd.read_html(url_fp_qb, header=1)[0]
+fp_qb['Site']='FantasyPros'
+
+url_fp_wr = "https://www.fantasypros.com/nfl/projections/wr.php?week=draft"
+fp_wr = pd.read_html(url_fp_wr, header=1)[0]
+fp_wr['Site']='FantasyPros'
+
+url_fp_rb = "https://www.fantasypros.com/nfl/projections/rb.php?week=draft"
+fp_rb = pd.read_html(url_fp_rb, header=1)[0]
+fp_rb['Site']='FantasyPros'
+
+url_fp_te = "https://www.fantasypros.com/nfl/projections/te.php?week=draft"
+fp_te = pd.read_html(url_fp_te, header=1)[0]
+fp_te['Site']='FantasyPros'
+
+# Empty Dataframes for app layout
+df = pd.DataFrame(columns=['Season', 'Passing Yds', 'Passing TDs', 'INTs', 'Rushing Yds', 'Rushing TDs', 'Fumbles', 'Receiving Yds', 'Receiving TDs', 'Receptions'])
+#projected_df = pd.DataFrame(columns=['REC'])
 
 # App layout
 app.layout = html.Div([
@@ -26,79 +47,103 @@ app.layout = html.Div([
         [
             dbc.Col(
                 [
-                    dbc.Row(html.Div('Passing', style={'textAlign': 'left', "text-decoration": "underline"})),
-                    dbc.Row(
+                    dbc.Card(
                         [
-                            dbc.Col(html.Div("Passing TD pts", style={'white-space': 'nowrap'}), width=6),
-                            dbc.Col(dcc.Input(id="pass_tds", type="number", placeholder=0, style={'width': '100%'}), width=2)
-                        ],
-                        className="mb-3"
-                    ),
-                    dbc.Row(
-                        [
-                            dbc.Col(html.Div("Passing YDs (per 1 yard)", style={'white-space': 'nowrap'}), width=6),
-                            dbc.Col(dcc.Input(id="pass_yds", type="number", placeholder=0, style={'width': '100%'}), width=2)
-                        ],
-                        className="mb-3"
-                    ),
-                    dbc.Row(
-                        [
-                            dbc.Col(html.Div("Interceptions", style={'white-space': 'nowrap'}), width=6),
-                            dbc.Col(dcc.Input(id="ints", type="number", placeholder=0, style={'width': '100%'}), width=2)
-                        ],
-                        className="mb-3"
+                            dbc.CardHeader(html.Div('Passing', style={'textAlign': 'left', "text-decoration": "underline"})),
+                            dbc.CardBody(
+                                [
+                                    dbc.Row(
+                                        [
+                                            dbc.Col(html.Div("Passing TD pts", style={'white-space': 'nowrap'}), width=6),
+                                            dbc.Col(dcc.Input(id="pass_tds", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                                        ],
+                                        className="mb-3"
+                                    ),
+                                    dbc.Row(
+                                        [
+                                            dbc.Col(html.Div("Passing YDs (per 1 yard)", style={'white-space': 'nowrap'}), width=6),
+                                            dbc.Col(dcc.Input(id="pass_yds", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                                        ],
+                                        className="mb-3"
+                                    ),
+                                    dbc.Row(
+                                        [
+                                            dbc.Col(html.Div("Interceptions", style={'white-space': 'nowrap'}), width=6),
+                                            dbc.Col(dcc.Input(id="ints", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                                        ],
+                                        className="mb-3"
+                                    ),
+                                ]
+                            ),
+                        ]
                     ),
                 ],
             ),
             dbc.Col(
                 [
-                    dbc.Row(html.Div('Rushing', style={'textAlign': 'left', "text-decoration": "underline"})),
-                    dbc.Row(
+                    dbc.Card(
                         [
-                            dbc.Col(html.Div("Rushing TD pts", style={'white-space': 'nowrap'}), width=6),
-                            dbc.Col(dcc.Input(id="rush_tds", type="number", placeholder=0, style={'width': '100%'}), width=2)
-                        ],
-                        className="mb-3"
-                    ),
-                    dbc.Row(
-                        [
-                            dbc.Col(html.Div("Rushing YDs (per 1 yard)", style={'white-space': 'nowrap'}), width=6),
-                            dbc.Col(dcc.Input(id="rush_yds", type="number", placeholder=0, style={'width': '100%'}), width=2)
-                        ],
-                        className="mb-3"
-                    ),
-                    dbc.Row(
-                        [
-                            dbc.Col(html.Div("Fumbles Lost", style={'white-space': 'nowrap'}), width=6),
-                            dbc.Col(dcc.Input(id="fumbles", type="number", placeholder=0, style={'width': '100%'}), width=2)
-                        ],
-                        className="mb-3"
+                            dbc.CardHeader(html.Div('Rushing', style={'textAlign': 'left', "text-decoration": "underline"})),
+                            dbc.CardBody(
+                                [
+                                    dbc.Row(
+                                        [
+                                            dbc.Col(html.Div("Rushing TD pts", style={'white-space': 'nowrap'}), width=6),
+                                            dbc.Col(dcc.Input(id="rush_tds", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                                        ],
+                                        className="mb-3"
+                                    ),
+                                    dbc.Row(
+                                        [
+                                            dbc.Col(html.Div("Rushing YDs (per 1 yard)", style={'white-space': 'nowrap'}), width=6),
+                                            dbc.Col(dcc.Input(id="rush_yds", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                                        ],
+                                        className="mb-3"
+                                    ),
+                                    dbc.Row(
+                                        [
+                                            dbc.Col(html.Div("Fumbles Lost", style={'white-space': 'nowrap'}), width=6),
+                                            dbc.Col(dcc.Input(id="fumbles", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                                        ],
+                                        className="mb-3"
+                                    ),
+                                ]
+                            ),
+                        ]
                     ),
                 ]
             ),
             dbc.Col(
                 [
-                    dbc.Row(html.Div('Receiving', style={'textAlign': 'left', "text-decoration": "underline"})),
-                    dbc.Row(
+                    dbc.Card(
                         [
-                            dbc.Col(html.Div("Receiving TD pts", style={'white-space': 'nowrap'}), width=6),
-                            dbc.Col(dcc.Input(id="rec_tds", type="number", placeholder=0, style={'width': '100%'}), width=2)
-                        ],
-                        className="mb-3"
-                    ),
-                    dbc.Row(
-                        [
-                            dbc.Col(html.Div("Receiving YDs (per 1 yard)", style={'white-space': 'nowrap'}), width=6),
-                            dbc.Col(dcc.Input(id="rec_yds", type="number", placeholder=0, style={'width': '100%'}), width=2)
-                        ],
-                        className="mb-3"
-                    ),
-                    dbc.Row(
-                        [
-                            dbc.Col(html.Div("Receptions (ppr)", style={'white-space': 'nowrap'}), width=6),
-                            dbc.Col(dcc.Input(id="recs", type="number", placeholder=0, style={'width': '100%'}), width=2)
-                        ],
-                        className="mb-3"
+                            dbc.CardHeader(html.Div('Receiving', style={'textAlign': 'left', "text-decoration": "underline"})),
+                            dbc.CardBody(
+                                [
+                                    dbc.Row(
+                                        [
+                                            dbc.Col(html.Div("Receiving TD pts", style={'white-space': 'nowrap'}), width=6),
+                                            dbc.Col(dcc.Input(id="rec_tds", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                                        ],
+                                        className="mb-3"
+                                    ),
+                                    dbc.Row(
+                                        [
+                                            dbc.Col(html.Div("Receiving YDs (per 1 yard)", style={'white-space': 'nowrap'}), width=6),
+                                            dbc.Col(dcc.Input(id="rec_yds", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                                        ],
+                                        className="mb-3"
+                                    ),
+                                    dbc.Row(
+                                        [
+                                            dbc.Col(html.Div("Receptions (ppr)", style={'white-space': 'nowrap'}), width=6),
+                                            dbc.Col(dcc.Input(id="recs", type="number", placeholder=0, style={'width': '100%'}), width=2)
+                                        ],
+                                        className="mb-3"
+                                    ),
+                                ]
+                            ),
+                        ]
                     ),
                 ]
             ),
@@ -118,13 +163,20 @@ app.layout = html.Div([
             dbc.Row([
                 dbc.Col(html.Div(id='image-container', style={'textAlign': 'center'}), width=4),
                 dbc.Col(
-                    dash_table.DataTable(
-                        df.to_dict('records'),
-                        [{"name": i, "id": i} for i in df.columns],
-                        id='classic_stats',
-                        style_table={'overflowX': 'auto'},
-                        style_cell={'textAlign': 'left'}
-                    ),
+                    html.Div([
+                        html.H3("Season Stats", style={'textAlign': 'center'}),
+                        dash_table.DataTable(
+                            id='classic_stats',
+                            style_table={'overflowX': 'auto'},
+                            style_cell={'textAlign': 'left'}
+                        ),
+                        html.H3("Projected Stats - 2024", style={'textAlign': 'center', 'marginTop': '20px'}),
+                        dash_table.DataTable(
+                            id='projected_stats',
+                            style_table={'overflowX': 'auto'},
+                            style_cell={'textAlign': 'left'}
+                        )
+                    ]),
                     width=8
                 )
             ])
@@ -132,9 +184,10 @@ app.layout = html.Div([
     ], style={'display': 'none'})
 ], style={'padding': '20px'})
 
-# Callback to update player name output and display image
+# Callback to update player name output, display image, and update stats tables
 @app.callback(
     [Output('classic_stats', 'data'),
+     Output('projected_stats', 'data'),
      Output('player_name_out', 'children'),
      Output('image-container', 'children'),
      Output('result-container', 'style')],
@@ -143,21 +196,43 @@ app.layout = html.Div([
 )
 def update_output(n_clicks, player_name):
     if n_clicks == 0:
-        return [], "", html.Div(), {'display': 'none'}  # Return empty data, empty name, and hide result container if button hasn't been clicked
+        return [], [], "", html.Div(), {'display': 'none'}  # Return empty data, empty name, and hide result container if button hasn't been clicked
     if not player_name:
-        return [], "No Player Selected", html.Div(), {'display': 'none'}  # Return empty data, empty name, and hide result container if no player name
+        return [], [], "No Player Selected", html.Div(), {'display': 'none'}  # Return empty data, empty name, and hide result container if no player name
     else:
         player_gsis_id = players[players['name/team/pos'] == player_name]['gsis_id'].unique()[0]
         df = seasonal_data[seasonal_data['player_id'] == player_gsis_id]
         image_url = players[players['name/team/pos'] == player_name]['headshot'].unique()[0]
-        if image_url:
-            response = requests.get(image_url)
-            if response.status_code == 200:
-                return df.to_dict('records'), f"You entered: {player_name}", html.Img(src=image_url, style={'width': '325px', 'height': '232px'}), {'display': 'block'}
-            else:
-                return df.to_dict('records'), f"Failed to retrieve the image for {player_name}. Status code: {response.status_code}", html.Div(), {'display': 'block'}
-        else:
-            return df.to_dict('records'), f"{player_name} does not have a headshot.", html.Div(), {'display': 'block'}
+        
+        # Get projected stats
+        player_name_split = player_name.split(' ')
+        player_name_only = player_name_split[0] + ' ' + player_name_split[1]
+        team = player_name_split[2]
+        projected_df = pd.DataFrame()
+        position = players[players['name/team/pos'] == player_name]['position'].unique()[0]
+        if position == 'QB':
+            projected_df = fp_qb[(fp_qb['Player'].str.contains(player_name_only)) & (fp_qb['Player'].str.contains(team))]
+            projected_df.columns = fp_qb.columns
+        elif position == 'WR':
+            projected_df = fp_wr[(fp_wr['Player'].str.contains(player_name_only)) & (fp_wr['Player'].str.contains(team))]
+            projected_df.drop(columns=['Player','FPTS','ATT'],inplace=True)
+            projected_df.rename(columns={'REC':'Receptions','YDS':'Receiving Yds','TDS':'Receiving TDs','YDS.1':'Rushing Yds','TDS.1':'Rushing TDs','FL':'Fumbles'},inplace=True)
+            projected_df=projected_df[['Site','Receiving Yds','Receptions','Receiving TDs','Rushing Yds','Rushing TDs','Fumbles']]
+            #projected_df.columns = fp_wr.columns.drop(column='Player')
+        elif position == 'RB':
+            projected_df = fp_rb[(fp_rb['Player'].str.contains(player_name_only)) & (fp_rb['Player'].str.contains(team))]
+            projected_df.columns = fp_rb.columns
+        elif position == 'TE':
+            projected_df = fp_te[(fp_te['Player'].str.contains(player_name_only)) & (fp_te['Player'].str.contains(team))]
+            projected_df.columns = fp_te.columns
+                
+        
+        
+        # Format data for dash_table.DataTable
+        classic_stats_data = df[['Season', 'Passing Yds', 'Passing TDs', 'INTs', 'Rushing Yds', 'Rushing TDs', 'Fumbles', 'Receiving Yds', 'Receptions']].to_dict('records')
+        projected_stats_data = projected_df.to_dict('records')
+        
+        return classic_stats_data, projected_stats_data, f"Player Selected: {player_name}", html.Img(src=image_url, style={'width': '100%', 'height': 'auto'}), {'display': 'block'}
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8050))
